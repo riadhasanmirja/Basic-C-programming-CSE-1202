5189b8f8c27d9276065e11284d13d09f0fdb5fa2
MD. Riad Hasan Mirja <riadrikto0357@gmail.com>
2026-01-18 11:54:17 +0600

    Add program for basic arithmetic operations

---
+++ Sum Difference product quotient and remainder.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+
+int a,b;
+
+printf("Enter your two Number :");
+scanf("%d %d",&a,&b);
+
+printf("Sum is %d\n",a+b);
+printf("Difference is %d\n",a-b);
+printf("Product is %d\n",a*b);
+printf("Quotient is %d\n",a/b);
+printf("Remainder is %d\n",a%b);
+
+return 0;
+}
