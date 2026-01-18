338c731b8afd18a3f1742ce7f856b2994ed74159
MD. Riad Hasan Mirja <riadrikto0357@gmail.com>
2026-01-18 12:51:24 +0600

    Create Celsius to Fahrenheit.c

---
+++ Celsius to Fahrenheit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+
+    int C,F;
+
+    printf("Enter Your Celsius Value :");
+    scanf("%d",&C);
+
+    printf("Fahrenheit value is :%d",F = ((C*9/5)+32));
+
+    return 0;
+}
