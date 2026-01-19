cf2634a4fc1d305596dbc8742062e53a92cb0ce3
MD. Riad Hasan Mirja <riadrikto0357@gmail.com>
2026-01-19 21:59:25 +0600

    Add program to calculate square and cube of a number

---
+++ Square and Cube.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+
+    printf("Enter Your Number :");
+    scanf("%d",&a);
+
+    printf("Square value is :%d\n Cube value is :%d\n",a*a,a*a*a);
+
+    return 0;
+}
