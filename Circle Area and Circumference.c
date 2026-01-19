47efaf861981e9e9e7fa971fa881ec376d6c33dd
MD. Riad Hasan Mirja <riadrikto0357@gmail.com>
2026-01-19 22:09:51 +0600

    Create Circle Area and Circumference.c

---
+++ Circle Area and Circumference.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+
+    float r,pi=3.1416;
+
+    printf("Enter Your Value :");
+    scanf("%f",&r);
+
+    printf("Circle Area :%f\n Circumference :%f\n",pi*r*r,2*pi*r);
+
+    return 0;
+}
